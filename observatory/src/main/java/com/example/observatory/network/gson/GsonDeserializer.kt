52d5f7c563bf9793c382f8b6bc439101ea0dc939
Ronald Chu <ronaldchu0118@gmail.com>
2025-03-04 19:02:48 +0800

    fix(Gson-Deserializer): fix recursive call on GsonConverterFactory

--- observatory/src/main/java/com/example/observatory/network/gson/GsonDeserializer.kt
+++ observatory/src/main/java/com/example/observatory/network/gson/GsonDeserializer.kt
@@ -1,21 +1,24 @@
 package com.example.observatory.network.gson
 
 import com.example.observatory.network.model.Uvindex
+import com.google.gson.Gson
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
 import java.lang.reflect.Type
 
-class GsonDeserializer : JsonDeserializer<Any> {
+class GsonDeserializer<T> : JsonDeserializer<T?> {
     override fun deserialize(
         json: JsonElement,
         typeOfT: Type,
         context: JsonDeserializationContext
-    ): Any? {
-        if (json.isJsonObject) {
-            return context.deserialize<Uvindex>(json, typeOfT)
+    ): T? {
+        return if (json.isJsonObject ) {
+            Gson().fromJson(json,typeOfT)
+        } else if (json.isJsonPrimitive && json.asJsonPrimitive.isString && json.asString.isEmpty()) {
+            null
+        } else {
+            null
         }
-
-        return null
     }
 }
